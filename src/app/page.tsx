aaa2a60348cbc88f35ceb8ccffe07e6dd74fd3e6
ccnspace <ccngithub@gmail.com>
2024-10-03 23:15:24 +0900

    개인 ocid 값을 가져오는 api 정의 및 api key값 환경변수 저장

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Contents } from "@/components/Contents";
+
 export default function Page() {
   return (
     <h1 className="text-2xl font-bold underline font-pretendard">
       안녕 nextjs!
+      <Contents />
     </h1>
   );
 }
---
+++ src/app/user/[name]/route.ts
@@ -0,0 +1,19 @@
+export const dynamic = "force-static";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { name: string } }
+) {
+  const username = params.name;
+  console.log(process.env.API_DOMAIN);
+  const res = await fetch(
+    `${process.env.API_DOMAIN}/id?character_name=${username}`,
+    {
+      headers: {
+        "x-nxopen-api-key": process.env.API_KEY || "",
+      },
+    }
+  );
+  const data = await res.json();
+  return Response.json({ data });
+}

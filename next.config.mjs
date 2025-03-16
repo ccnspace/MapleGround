bd8f564c08f2fb272a3aad924044f40ff3cf8ac3
ccnspace <ccngithub@gmail.com>
2025-03-17 01:37:56 +0900

    mp3 파일을 읽기 위한 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,20 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp3|wav)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/sounds/",
+          outputPath: "static/sounds/",
+          name: "[name].[ext]",
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
